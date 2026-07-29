b3b1f774da97fa5aaf6a29a47c97c161191dd20a
HamzahHuzaifah <hamzahhuzaifah1945@gmail.com>
2026-07-29 22:49:19 +0700

    Commit lagi

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 // Konfigurasi: Maksimal 50 submit dari 1 IP yang sama dalam waktu 15 menit (Dinaikkan sementara untuk keperluan testing)
 const pendaftaranLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 menit
-    max: 50, // Batas maksimal request per IP (Default produksi disarankan 5)
+    max: 5, // Batas maksimal request per IP (Default produksi disarankan 5)
     message: 'Terlalu banyak permintaan pendaftaran dari komputer ini. Silakan coba lagi setelah 15 menit.',
     standardHeaders: true, // Mengirim info rate limit di header (standar baru)
     legacyHeaders: false, // Mematikan header standar lama
